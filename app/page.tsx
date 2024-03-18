0eea120ec1a6c1eafac3a8eddcedfb45b11d361b
Explonentielle <126072458+Explonentielle@users.noreply.github.com>
2024-03-18 23:05:51 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/src/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p>test</p>
+      <Button>test</Button>
+    </div>
+  );
+}
