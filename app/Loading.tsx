fa28730b88451b011f9e0338a66c310fe747a2c3
Explonentielle <126072458+Explonentielle@users.noreply.github.com>
2024-04-19 22:24:00 +0200

    'locationFeatureAddd'

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -4,7 +4,7 @@ import { Layout, LayoutTitle } from "@/src/components/Layout";
 export default function Loading() {
     return (
         <Layout>
-            <LayoutTitle className={'flex items-center justify-center'}>
+            <LayoutTitle className={'flex h-full items-center justify-center'}>
                 ... Loading
             </LayoutTitle>
         </Layout>)
