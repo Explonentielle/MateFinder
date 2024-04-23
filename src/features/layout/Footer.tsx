1d40a77fde7e23e3ef9f2220f7e156de42689473
Explonentielle <126072458+Explonentielle@users.noreply.github.com>
2024-04-23 15:34:44 +0200

    dateCorrectiond

--- src/features/layout/Footer.tsx
+++ src/features/layout/Footer.tsx
@@ -14,17 +14,17 @@ export const Footer = async () => {
                     </span>
                     <ul className="mt-3 flex flex-wrap items-center gap-4 text-sm font-medium text-muted-foreground sm:mt-0">
                         <li>
-                            <a href="#pricing" className="hover:underline">
+                            <a href="#" className="hover:underline">
                                 Pricing
                             </a>
                         </li>
                         <li>
-                            <a href="#features" className="hover:underline">
+                            <a href="#" className="hover:underline">
                                 Features
                             </a>
                         </li>
                         <li>
-                            <a href="/products" className="hover:underline">
+                            <a href="/" className="hover:underline">
                                 App
                             </a>
                         </li>
