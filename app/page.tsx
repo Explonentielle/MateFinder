dcf30ec086e377652c0ff95fe0e9b1739694b770
Explonentielle <126072458+Explonentielle@users.noreply.github.com>
2024-04-23 15:30:22 +0200

    Avataraddkis

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default async function Home() {
   if (!user) {
     return (
       <div className="size-full">
-        l
         <Header />
         <LocationForm />
         <BannerLanding />
