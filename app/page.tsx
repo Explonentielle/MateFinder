7fcffa905286c038b48217bdf09c37f47bc2a97f
Explonentielle <126072458+Explonentielle@users.noreply.github.com>
2024-04-24 22:03:09 +0200

    upd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Header } from "@/src/features/layout/Header";
 import { currentUser } from "@/src/auth/current-user";
 import { LocationForm } from "@/src/features/landing/LocationForm";
-import ProfileUpdateForm from "@/src/features/landing/ProfilUpdateForm";
 import { FAQSection } from "@/src/features/landing/FAQsection";
 import { BannerLanding } from "@/src/features/landing/BannerLanding";
 import { Footer } from "@/src/features/layout/Footer";
@@ -14,59 +13,9 @@ export default async function Home() {
 
   const user = await currentUser();
 
-
   if (!user) {
     redirect("/landing/location");
   } else {
     redirect("/landing");
   }
-
-  // if (!user) {
-  //   redirect("/landing/location");
-  // } 
-  // else if (user && user.location) {
-  //   redirect("/landing");
-  // } 
-  // else {
-  //   redirect("/profil");
-  // }
-  
-
-
-  // if (!user) {
-  //   return (
-  //     <div className="size-full">
-  //       <Header />
-  //       <LocationForm />
-  //       <BannerLanding />
-  //       <FAQSection />
-  //       <Layout>
-  //         <Footer />
-  //       </Layout>
-  //     </div>
-  //   )
-  // }
-  // else if (user && (!user?.username || !user.age || !user.name || !user.location)) {
-  //   return (
-  //     <div className="size-full">
-  //       <Header />
-  //       <Layout>
-  //         <LandingHero />
-  //         <ProfileUpdateForm userId={user?.id} />
-  //       </Layout>
-  //     </div>
-  //   )
-
-  // } else {
-  //   if (!user?.location) {
-  //     throw Error
-  //   }
-
-  //   return (
-  //     <div className="size-full">
-  //       <Header />
-  //       {/* <Landing location={user.location} /> */}
-  //     </div>
-  //   );
-  // }
 }
